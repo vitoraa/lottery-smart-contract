59a9119832f13b6189804cca397ae9faa62a5025
vitoraa <vitor.lima@outlook.com>
2022-06-18 17:05:50 -0300

    initial commit

---
+++ deploy.js
@@ -0,0 +1,26 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+ 
+const { abi, evm } = require('./compile');
+ 
+provider = new HDWalletProvider(
+    'region range hold discover relief that consider thunder priority explain receive cactus',
+    'https://rinkeby.infura.io/v3/7fae2aed999247a8935ebff226d3e03b'
+);
+ 
+const web3 = new Web3(provider);
+ 
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+ 
+  console.log('Attempting to deploy from account', accounts[0]);
+ 
+  const result = await new web3.eth.Contract(abi)
+    .deploy({ data: evm.bytecode.object, arguments: ['Hi there!'] })
+    .send({ gas: '1000000', from: accounts[0] });
+ 
+  console.log('Contract deployed to', result.options.address);
+  provider.engine.stop();
+};
+ 
+deploy();
